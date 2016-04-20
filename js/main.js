2bf3d46e9bc4123faf3518a7d2fcb07b73bad07b
retornam <retornam@collectivehealth.com>
2016-04-20 15:37:50 -0700

    skeletons

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+
+/*
+Given http://httpbin.org/ip as an API endpoint that returns your current IP,
+write a JavaScript function that makes a call to the endpoint, gets the value
+returned and displays it on an HTML page.
+
+How will you test this? */
